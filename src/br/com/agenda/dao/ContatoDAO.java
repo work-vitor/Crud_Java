c1e52c0a178701ef5c2e5c23a48c356410669bfd
joao_vitor <joa.fggf3.27@gmail.com>
2021-06-21 11:20:49 -0300

    Version 2

---
+++ src/br/com/agenda/dao/ContatoDAO.java
@@ -0,0 +1,68 @@
+package br.com.agenda.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import br.com.agenda.factory.ConnectionFactory;
+import br.com.agenda.model.Contato;
+
+public class ContatoDAO {
+	
+	/*
+	*c: create
+	*r: read
+	*u: update
+	*d: delete
+	*/
+	
+	public void save(Contato contato) {
+		
+		String sql = "INSERT INTO contatos(nome, idade, datacadastro) VALUES (?, ?, ?) ";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			
+			//Cria uma conexeão com o banco de dados
+			conn = ConnectionFactory.createConnectionToMySQL();//Chamando a conexão
+			
+			//Criamos uma PreparedStatement, para executar uma query
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+
+			//Adicionar os valores que são esperados pela query
+			pstm.setString(1, contato.getNome());
+			pstm.setInt(2, contato.getIdade());
+			pstm.setDate(3, new Date(contato.getDataCadastro().getTime()));
+			
+			//Executar a query
+			pstm.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			//Fechar as conexões
+			try {
+				
+				if (pstm!=null) {
+					pstm.close();
+				}
+				
+				if (conn!=null) {
+					conn.close();
+				}
+				
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+				
+			}
+		}
+		
+		
+	}
+	
+	
+}
